7fe852e9d63c4080847c7e8b564afe8bc85e18ee
Naif Otibi <nn.n@outlook.cl>
2024-11-07 17:47:51 +0300

    adding translate value its much much better

--- src/app/components/Progress.tsx
+++ src/app/components/Progress.tsx
@@ -6,10 +6,10 @@ type ProgressProps = {
 
 
 export default function Progress({value}: ProgressProps){
-    const widthOfChildren = 400 - value;
+    const translateValue
     return (
         <div role="progressbar" className="w-[400px] h-[80px] border rounded-full border-indigo-500" aria-valuemin={0} aria-valuemax={100} aria-valuenow={value}>
-            <div className={`w-[${widthOfChildren}px] h-full bg-indigo-500 rounded`}></div>
+            <div className={`w-full h-full translate-x-[] bg-indigo-500 rounded-full`}></div>
         </div>
     )
 }
