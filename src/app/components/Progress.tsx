24b7a91e29f027026f492f68b2286d722b793c3a
Naif Otibi <nn.n@outlook.cl>
2024-11-07 17:44:54 +0300

    adding some style and crafting the width of children

--- src/app/components/Progress.tsx
+++ src/app/components/Progress.tsx
@@ -6,9 +6,10 @@ type ProgressProps = {
 
 
 export default function Progress({value}: ProgressProps){
+    const widthOfChildren = 400 - value;
     return (
-        <div role="progressbar" aria-valuemin={0} aria-valuemax={100} aria-valuenow={value}>
-            <div className="w-[]"></div>
+        <div role="progressbar" className="w-[400px] h-[80px] border rounded-full border-indigo-500" aria-valuemin={0} aria-valuemax={100} aria-valuenow={value}>
+            <div className={`w-[${widthOfChildren}px] h-full bg-indigo-500 rounded`}></div>
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import Progress from "./components/Progress";
 
 export default function Home() {
   return (
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-      
+      <Progress value={50} />
     </div>
   )
 }
