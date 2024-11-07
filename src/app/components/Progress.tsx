17dfffa218da0ff90f33709034b87f87404b944e
Naif Otibi <nn.n@outlook.cl>
2024-11-07 18:02:07 +0300

    adjusting the translate much better

--- src/app/components/Progress.tsx
+++ src/app/components/Progress.tsx
@@ -6,10 +6,10 @@ type ProgressProps = {
 
 
 export default function Progress({value}: ProgressProps){
-    const translateValue = 100 - value;
+    const translateValue = value - 100;
     return (
         <div role="progressbar" className="relative w-[400px] overflow-hidden h-[80px] border rounded-full border-indigo-500" aria-valuemin={0} aria-valuemax={100} aria-valuenow={value}>
-            <div className={`w-full h-full -translate-x-[${translateValue}%] bg-indigo-500 rounded-full`}></div>
+            <div className="w-full h-full bg-indigo-500" style={{translate: `-${value}%`}}></div>
         </div>
     )
 }
