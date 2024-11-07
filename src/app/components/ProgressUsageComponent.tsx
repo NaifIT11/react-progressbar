e7a0865808bbd637a13e032da635848f89231bec
Naif Otibi <nn.n@outlook.cl>
2024-11-07 19:22:41 +0300

    completing the animation

--- src/app/components/ProgressUsageComponent.tsx
+++ src/app/components/ProgressUsageComponent.tsx
@@ -1,6 +1,28 @@
 "use client"
 
 
+import React from 'react'
+import { useEffect, useState } from 'react';
+import Progress from './Progress';
+
 export default function ProgressUsage(){
+
+    const [progress,setProgress]=useState(0);
+  
+
+  useEffect(()=>{
+    if(progress<100){
+     const timer=setTimeout(()=>{
+      const p=progress+1;
+      setProgress(p);
+      console.log(p);
+     },100) 
+    }
+
     
+  },[progress]);
+  return (
+    <Progress value={progress} />
+  )
+
 }
